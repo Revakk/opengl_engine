ba57c6aa083286ad3f35ea3fdb968b9d0babce4a
Revakhoron <Sevcik.jan42@seznam.cz>
2023-03-10 16:46:47 +0100

    window initialization

--- opengl_engine/main.cpp
+++ opengl_engine/main.cpp
@@ -6,8 +6,70 @@ const GLint WIDTH = 800, HEIGHT = 600;
 
 int main()
 {
-    
+    //initialize GLFW
+    if (!glfwInit())
+    {
+        std::cout << "GLFINIT failed" << '\n';
+        glfwTerminate();
+        return 1;
+    }
 
+    // setup glfw window properties
+    // opengl version
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Core profile = no backwards compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Allow forward compatibility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* main_window = glfwCreateWindow(WIDTH, HEIGHT, "Test window", nullptr, nullptr);
+
+    if (!main_window)
+    {
+        std::cout << "GLFW window creation failed" << '\n';
+        glfwTerminate();
+        return 1;
+    }
+
+    // get buffer size information, later used for viewport
+    int buffer_width, buffer_height;
+    glfwGetFramebufferSize(main_window, &buffer_width, &buffer_height);
+
+    // set context for GLEW to use
+    glfwMakeContextCurrent(main_window);
+
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK) 
+    {
+        std::cout << "glew initialization failed" << '\n';
+        glfwDestroyWindow(main_window);
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup viewport size
+
+    glViewport(0, 0, buffer_width, buffer_height);
+
+    // Loop until window closed
+    while (!glfwWindowShouldClose(main_window))
+    {
+        // get + handle user input events
+
+        glfwPollEvents();
+
+        // clear window
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(main_window);
+    }
 
     return 0;
 }
